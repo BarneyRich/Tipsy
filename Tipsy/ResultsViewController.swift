98fbbc2220c7d47109a162f5d2c4531c61988ce7
Bala Barnabas Simon <barnabassimon486@gmail.com>
2022-01-31 16:44:41 +0300

    Fixed bugs. App completed.

--- Tipsy/ResultsViewController.swift
+++ Tipsy/ResultsViewController.swift
@@ -11,15 +11,21 @@ class ResultsViewController: UIViewController {
 
     @IBOutlet weak var totalLable: UILabel!
     @IBOutlet weak var settingsLable: UILabel!
-    override func viewDidLoad()
     
+    var result = "0.0"
+    var tip = 10
+    var split = 2
     
-    {
+    override func viewDidLoad(){
         super.viewDidLoad()
         
-    }
+        totalLable.text = result
+        settingsLable.text = "Split between \(split) people, with \(tip)% tip."
+        }
 
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
     }
     
 }
