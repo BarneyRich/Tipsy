ff708ca6967268a4d20008891d432d30d9859f15
Bala Barnabas Simon <barnabassimon486@gmail.com>
2022-01-31 16:18:45 +0300

    Layout completed and Views and controllers implemented.

---
+++ Tipsy/CalculatorViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Barnabas Bala on 31.01.2022.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var bilTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLable: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    
+    
+
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Tipsy/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Barnabas Bala on 31.01.2022.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLable: UILabel!
+    @IBOutlet weak var settingsLable: UILabel!
+    override func viewDidLoad()
+    
+    
+    {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
